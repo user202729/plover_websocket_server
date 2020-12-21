2c98177b937a6b01c73a3ec271b689d3874913c2
user202729 <25191436+user202729@users.noreply.github.com>
2020-12-21 11:51:10 +0700

    Handle clients that close abruptly properly

--- plover_engine_server/websocket/views.py
+++ plover_engine_server/websocket/views.py
@@ -1,6 +1,7 @@
 """The views / handlers for the server."""
 
 from aiohttp import web, WSMsgType
+import asyncio
 
 
 async def index(request: web.Request) -> web.Response:
@@ -23,29 +24,36 @@ async def websocket_handler(request: web.Request) -> web.WebSocketResponse:
     print('WebSocket connection starting', flush=True)
     socket = web.WebSocketResponse()
     await socket.prepare(request)
-    request.app['websockets'].append(socket)
+    sockets = request.app['websockets']
+    sockets.append(socket)
     print('WebSocket connection ready', flush=True)
 
-    async for message in socket:
-        if message.type == WSMsgType.TEXT:
-            if message.data == 'close':
-                await socket.close()
-                continue
+    try:
+        async for message in socket:
+            if message.type == WSMsgType.TEXT:
+                if message.data == 'close':
+                    await socket.close()
+                    continue
 
-            import json
-            try:  # NOTE is this good API? What if message is not JSON/dict?
-                data = json.loads(message.data)
-            except json.decoder.JSONDecodeError:
-                print('Receive unknown data: ', message.data)
-                continue
+                import json
+                try:  # NOTE is this good API? What if message is not JSON/dict?
+                    data = json.loads(message.data)
+                except json.decoder.JSONDecodeError:
+                    print('Receive unknown data: ', message.data)
+                    continue
 
-            if isinstance(data, dict):
-                callback = request.app['on_message_callback']
-                callback(data)
-        elif message.type == WSMsgType.ERROR:
-            print('WebSocket connection closed with exception '
-                  f'{socket.exception()}', flush=True)
+                if isinstance(data, dict):
+                    callback = request.app['on_message_callback']
+                    callback(data)
+            elif message.type == WSMsgType.ERROR:
+                print('WebSocket connection closed with exception '
+                      f'{socket.exception()}', flush=True)
+    except asyncio.CancelledError:  # https://github.com/aio-libs/aiohttp/issues/1768
+        pass
+    finally:
+        await socket.close()
 
-    request.app['websockets'].remove(socket)
+
+    sockets.remove(socket)
     print('WebSocket connection closed', flush=True)
     return socket
